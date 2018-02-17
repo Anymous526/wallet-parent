e275904c95d6dfe97545dcd0bd99f5b5d128298c
Anymous526 <kevin.sun526@qq.com>
2018-02-17 09:28:23 +0800

    add user interface

--- api/src/main/java/live/wallet/service/UserManager.java
+++ api/src/main/java/live/wallet/service/UserManager.java
@@ -1,5 +1,10 @@
 package live.wallet.service;
 
+import live.wallet.model.User;
+
 public interface UserManager {
-	String sayHello(String name);
+	boolean deleteUser(User user);
+	boolean createUser(User user);
+	boolean updateProfile(User user);
+	 
 }
--- service/src/main/java/live/wallet/service/UserManagerImpl.java
+++ service/src/main/java/live/wallet/service/UserManagerImpl.java
@@ -2,13 +2,29 @@ package live.wallet.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
+import live.wallet.model.User;
+
 @Service
 public class UserManagerImpl implements UserManager {
 
-	@Override
-	public String sayHello(String name) {
-		return "Hello:" + name;
+	
 
+	@Override
+	public boolean deleteUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean createUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProfile(User user) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
