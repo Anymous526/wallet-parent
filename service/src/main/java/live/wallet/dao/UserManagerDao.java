5f2c1692189e447747682797addb9ef6cd9291a5
Anymous526 <kevin.sun526@qq.com>
2018-04-06 11:49:43 +0800

    dubbo

--- service/src/main/java/live/wallet/dao/UserManagerDao.java
+++ service/src/main/java/live/wallet/dao/UserManagerDao.java
@@ -1,9 +1,12 @@
 package live.wallet.dao;
 
+import org.springframework.stereotype.Repository;
+
 import live.wallet.model.User;
 
+@Repository
 public interface UserManagerDao {
 
-	boolean deleteUser(User user);
+    boolean deleteUser(User user);
 
 }
